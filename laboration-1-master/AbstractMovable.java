d010e8fceccf951929723f640364b0ee5d7484b8
Alvuk <alexvuckovic@hotmail.com>
2023-01-29 18:01:46 +0100

    1.0

--- laboration-1-master/AbstractMovable.java
+++ laboration-1-master/AbstractMovable.java
@@ -9,6 +9,13 @@ public abstract class AbstractMovable implements Movable {
     private Direction dir; // Direction of the car
     public double currentSpeed; // The current speed of the car
 
+    /**
+     * Constructor to initialize AbstractMovable
+     * @param xPos x-coordinate for the object
+     * @param yPos y-coordinate for the object
+     * @param dir Direction of the object
+     * @param currentSpeed The current speed of the object
+     */
     public AbstractMovable(double xPos, double yPos, Direction dir, double currentSpeed){
         this.xPos = xPos;
         this.yPos = yPos;
@@ -16,26 +23,58 @@ public abstract class AbstractMovable implements Movable {
         this.currentSpeed = currentSpeed;
     }
 
+    /**
+     * Returns xPos of the car
+     * @return xPos
+     */
     public double getxPos(){ return xPos; }
 
+    /**
+     * Sets xPos = x of the car
+     * @param x
+     */
     public void setxPos(double x){ xPos = x; }
 
+    /**
+     * Returns xPos of the car
+     * @return yPos
+     */
     public double getyPos(){
         return yPos;
     }
 
+    /**
+     * Sets yPos = y of the car
+     * @param y
+     */
     public void setyPos(double y){
         yPos = y;
     }
 
+    /**
+     * Returns the currentspeed of the car
+     * @return currentSpeed
+     */
     public double getCurrentSpeed(){
         return currentSpeed;
     }
 
+    /**
+     * Sets currentspeed based on amount
+     * @param amount
+     */
     void setCurrentSpeed(double amount){ currentSpeed = amount; }
 
+    /**
+     * Returns direction dir of car
+     * @return dir
+     */
     public Direction getDir(){return dir;}
 
+    /**
+     * The move method moves the object in the direction specified by the dir variable.
+     * The movement is based on the current speed of the object.
+     */
     @Override
     public void move() {
         switch (dir) {
@@ -46,6 +85,9 @@ public abstract class AbstractMovable implements Movable {
         }
     }
 
+    /**
+     * The turnLeft method turns the object in the direction specified by the dir variable.
+     */
     @Override
     public void turnLeft() {
         switch (dir) {
@@ -56,6 +98,9 @@ public abstract class AbstractMovable implements Movable {
         }
     }
 
+    /**
+     * The turnRight method turns the object in the direction specified by the dir variable.
+     */
     @Override
     public void turnRight() {
         switch (dir) {
--- test/AbstractMovableTest.java
+++ test/AbstractMovableTest.java
@@ -6,12 +6,16 @@ import static org.junit.Assert.assertEquals;
 public class AbstractMovableTest {
 
     private Car saab95;
+    private Car volvo240;
 
+    // Write Junit test for Volvo240
     @Before
     public void init() {
         saab95 = new Saab95(1,1, Direction.NORTH, 0);
+        volvo240 = new Volvo240(1,1,Direction.NORTH,0);
 
         saab95.startEngine();
+        volvo240.startEngine();
     }
 
     @Test
@@ -20,6 +24,11 @@ public class AbstractMovableTest {
         saab95.setCurrentSpeed(-saab95.getCurrentSpeed());
         saab95.move();
         assertEquals(1, saab95.getyPos(), 0.0);
+
+        volvo240.move();
+        volvo240.setCurrentSpeed(-volvo240.getCurrentSpeed());
+        volvo240.move();
+        assertEquals(1, volvo240.getxPos(), 0.0);
     }
 
     @Test
