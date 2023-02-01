fe27925fc1a57e8997de127b27e620c552050803
JibrilKewi <dominator123@hotmail.se>
2023-02-01 18:45:49 +0100

    v1.3 - Added documentation

--- laboration-1-master/Truck.java
+++ laboration-1-master/Truck.java
@@ -1,7 +1,9 @@
 import java.awt.*;
+/**
+ * Truck holds the specifics for a car object.
+ */
 
 public abstract class Truck extends Vehicle{
-    //ToDo Last-funktion
 
     Truck(int nrDoors, double enginePower, Color color, String modelName,
           double xPos, double yPos, Direction dir, double currentSpeed){
@@ -9,11 +11,18 @@ public abstract class Truck extends Vehicle{
         super(nrDoors, enginePower, color, modelName, xPos, yPos, dir, currentSpeed);
 
     }
-
+    /**
+     * isMoving() is a method for determening if a truck-object is moving.
+     * @return Returns true if truck is moving otherwise false.
+     */
     public boolean isMoving(){
         return getCurrentSpeed() > 0;
     }
 
+    /**
+     * speedFactor() calculates the speed-factor based on engine-power.
+     * @return Returns the speed-factor.
+     */
     @Override
     public double speedFactor(){
         return getEnginePower() * 0.01;
